8d0f8d11673efa07e76ea9fa565aa04d4f2fd8fe
pab-h <dev.pab.2020@gmail.com>
2026-02-16 12:04:28 -0300

    feat: waterFountain seeder

---
+++ prisma/seed.ts
@@ -0,0 +1,43 @@
+import { prisma } from "../src/lib/prisma"
+
+async function main() {
+
+    await prisma.waterFountain.create({
+      data: {
+        name: "Mucambinho",
+        location: "Recepção do Mucambinho",
+        water_consumptions: {
+          create: [
+            { volume: 1 },
+            { volume: 10 },
+            { volume: 5 },
+            { volume: 9 },
+          ]
+        },
+        water_temperatures: {
+          create: [
+            { temperature: 22 },
+            { temperature: 21 },
+            { temperature: 20 },
+            { temperature: 25 },
+          ]
+        },
+        filter_changes: {
+          create: [
+            { created_at:new Date("1997") }
+          ]
+        }
+      }
+    })
+
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
