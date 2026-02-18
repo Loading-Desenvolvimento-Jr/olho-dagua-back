aa65e95204e8b69bf8beeb337a1011be4edb4d0c
pab-h <dev.pab.2020@gmail.com>
2026-02-18 12:19:59 -0300

    refactor: shared folder

---
+++ src/shared/env.ts
@@ -0,0 +1,27 @@
+import { config } from 'dotenv';
+import { z }      from 'zod';
+
+config();
+
+const envSchema = z.object({
+
+    NODE_ENV:     z.string(),
+
+    APP_PORT:     z.coerce.number(),
+    APP_HOST:     z.string(),
+    
+    DATABASE_URL: z.string(),
+
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (!_env.success) {
+    
+  console.error(_env.error);
+
+  throw new Error('Invalid environment variables');
+
+}
+
+export const env = _env.data;
