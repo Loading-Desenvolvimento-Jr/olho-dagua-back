510d781c13f664179870962df84be2b0a01743a9
Cizé Lucas <cizelucas@gmail.com>
2026-03-13 19:03:21 -0300

    feat: enhance error handling to include Fastify error responses

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,6 +1,9 @@
 import { FastifyError, FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { AppError } from "../shared/exception/AppError";
 import { ZodError } from "zod";
+import { PrismaClientRustPanicError } from "@prisma/client/runtime/client";
+import Fastify from 'fastify';
+import { errorCodes } from 'fastify'
 
 export interface ErrorResponse {
     statusCode: number;
@@ -38,6 +41,27 @@ export function ErrorHandlerMiddleware(
         return reply
             .status(400)
             .send(buildErrorResponse(400, "Bad Request", details));
+    }
+
+    // Catch Fastify's errors
+    if (error.code?.startsWith('FST_')) {
+        const status = error.statusCode ?? 500;
+
+        app.log.error({
+            request: {
+                method: request.method,
+                url: request.url,
+                headers: request.headers,
+                body: request.body,
+                query: request.query,
+                parameters: request.params,
+            },
+            error: error,
+        }, "A Fastify related error occurred");
+
+        return reply
+            .status(status)
+            .send(buildErrorResponse(status, error.code, "An unknown error occurred."));
     }
 
     // For unexpected / unknown errors
