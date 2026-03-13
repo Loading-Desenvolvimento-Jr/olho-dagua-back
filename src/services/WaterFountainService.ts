512503201b9a5d8809afc2d1168a0a4cc9df56a2
pab-h <dev.pab.2020@gmail.com>
2026-03-13 14:58:33 -0300

    feat: update method on WaterFountainService

--- src/services/WaterFountainService.ts
+++ src/services/WaterFountainService.ts
@@ -1,5 +1,6 @@
 import { WaterFountain }           from "../../prisma/generated/client";
 import { WaterFountainRepository } from "../repositories/interfaces/WaterFountainRepository";
+import { ResourceNotFound } from "../shared/exception/ResourceNotFound";
 
 export class WaterFountainService {
 
@@ -12,11 +13,26 @@ export class WaterFountainService {
     }
 
     public async findById(waterFountainId: string) {
-        return this.repository.findById(waterFountainId);
+        return await this.repository.findById(waterFountainId);
     }
 
     public async findAll(): Promise<WaterFountain[]> {
-        return this.repository.findAll();
+        return await this.repository.findAll();
+    }
+
+    public async update(waterFountain: WaterFountain): Promise<WaterFountain> {
+
+        const waterFountainFound = await this.findById(
+            waterFountain.id
+        );
+
+        if (!waterFountainFound) {
+            throw new ResourceNotFound(
+                `Water fountain with id ${waterFountain.id} not found`
+            );
+        }
+
+        return await this.repository.update(waterFountain);
     }
 
 }
